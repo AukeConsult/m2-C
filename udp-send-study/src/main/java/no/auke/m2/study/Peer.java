3fcfec379debaa38322a60945da3bb0d1a21d2a9
Leif Auke <leif.auke@akersolutions.com>
2023-09-01 11:58:11 +0200

    more initial dev, enable classes

--- udp-send-study/src/main/java/no/auke/m2/study/Peer.java
+++ udp-send-study/src/main/java/no/auke/m2/study/Peer.java
@@ -1,6 +1,15 @@
 package no.auke.m2.study;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Peer {
+
+    public List<Udp> outgoing = new ArrayList<>();
+    public BlockingQueue<Udp> incoming = new LinkedBlockingDeque<>();
 
     public Peer(String id) {
 
